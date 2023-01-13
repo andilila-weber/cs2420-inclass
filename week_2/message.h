873e618c3e89a3e875c511eed3aae1a88fb69bf5
nineil-weber <109441984+nineil-weber@users.noreply.github.com>
2023-01-13 11:09:15 -0700

    Week2

---
+++ week_2/message.h
@@ -0,0 +1,23 @@
+//
+// Created by NilsMurrugarra-Llerena on 1/2/23.
+//
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Message
+{
+    private:
+        string msg;
+
+    public:
+        Message(string msg): msg(msg) {}
+        void show(){ cout<<msg<<endl; }
+        void set_message(string msg){this->msg = msg;}
+        unique_ptr<Message> deep_copy()
+        {
+            auto dc = make_unique<Message>(msg);
+            return dc;
+        }
+};
